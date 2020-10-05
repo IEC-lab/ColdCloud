cdde6228ddd9424071f3570ff7c0c44a2a08c952
unknown <770340084@qq.com>
2020-10-05 12:48:54 +0800

    modify api url

--- src/components/Common.vue
+++ src/components/Common.vue
@@ -1,9 +1,7 @@
 <script>
-    const FEURL = "http://localhost:8080"
-    const APIURL = FEURL + "/api"
+    const APIURL = "/api"
     const STREAMSURL = APIURL + "/streams"
     export default {
-        FEURL,
         APIURL,
         STREAMSURL
     }
