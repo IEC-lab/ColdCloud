465c2cd5bb3f31334272e6118cc4bb0fa4818fce
unknown <770340084@qq.com>
2020-10-05 17:55:10 +0800

    modify ports

--- src/components/Common.vue
+++ src/components/Common.vue
@@ -1,7 +1,7 @@
 <script>
     const APIURL = "/api"
     const STREAMSURL = APIURL + "/streams"
-    const COLDBRIDGEURL = "ws://192.168.1.4:8888/rtsp/1/"
+    const COLDBRIDGEURL = "ws://192.168.1.4:9000/rtsp/1/"
     export default {
         APIURL,
         STREAMSURL,
