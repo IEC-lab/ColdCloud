4174f43486030ba0277b64fb5ce2a663ff58c3f8
unknown <770340084@qq.com>
2020-10-06 11:32:25 +0800

    support graphql

--- src/App.vue
+++ src/App.vue
@@ -7,52 +7,45 @@
 </template>
 
 <script>
-import $ from "jquery"
-import Common from "@/components/Common"
-import flvjs from "flv.js"
+import $ from "jquery";
+import Common from "@/components/Common";
+import flvjs from "flv.js";
+import graphqlHelper from "@/pkg/Graphql";
 
 export default {
   name: "App",
   data() {
-    var oneUrl = ""
-    $.ajax({
-      type: "GET",
-      async: false,
-      url: Common.STREAMSURL,
-      success: function(data) {
-        oneUrl = data.streams[0]
-      },
-      error: function(err) {
-        console.log(err.status)
-        console.log(err.responseText)
-      }
-    })
-    return {
-      testUrl: oneUrl
+    var rs = graphqlHelper.queryGraphql(`query Query {getFrameStreams}`);
+    var oneUrl = "";
+    if(rs != null){
+      oneUrl = rs.getFrameStreams[0];
     }
+    return {
+      testUrl: oneUrl,
+    };
   },
-  mounted: function() {
-    var theObj = this
-    var testVideo = $("#testVideo")[0]
+  mounted: function () {
+    var theObj = this;
+    var testVideo = $("#testVideo")[0];
 
     if (flvjs.isSupported()) {
       theObj.player = flvjs.createPlayer({
         type: "flv",
         isLive: true,
-        url: `${Common.COLDBRIDGEURL}?url=${theObj.testUrl}`
-      })
-      theObj.player.attachMediaElement(testVideo)
+        url: `${Common.COLDBRIDGEURL}?url=${theObj.testUrl}`,
+      });
+      theObj.player.attachMediaElement(testVideo);
       try {
-        theObj.player.load()
-        theObj.player.play()
+        theObj.player.load();
+        theObj.player.play();
       } catch (error) {
-        console.log(error)
+        console.log(error);
       }
-    }else{
-      alert("flvjs isn't supported")
+    } else {
+      alert("flvjs isn't supported");
     }
-  }
-}
+  },
+};
 </script>
 
 <style>
--- src/components/Common.vue
+++ src/components/Common.vue
@@ -1,10 +1,12 @@
 <script>
     const APIURL = "/api"
     const STREAMSURL = APIURL + "/streams"
+    const GRAPHQLURL = APIURL + "/graphql/query"
     const COLDBRIDGEURL = "ws://192.168.1.4:9000/rtsp/1/"
     export default {
         APIURL,
         STREAMSURL,
+        GRAPHQLURL,
         COLDBRIDGEURL
     }
 </script>
