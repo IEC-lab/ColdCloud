1a828381c6f5bf4854c9c398e9cc98d2785127b7
unknown <770340084@qq.com>
2020-12-16 08:35:58 +0800

    修改 COLDBRIDGEURL

--- src/components/Common.vue
+++ src/components/Common.vue
@@ -2,7 +2,7 @@
     const APIURL = "/api"
     const STREAMSURL = APIURL + "/streams"
     const GRAPHQLURL = APIURL + "/graphql/query"
-    const COLDBRIDGEURL = "ws://192.168.1.4:8888/rtsp/1/"
+    const COLDBRIDGEURL = "ws://211.67.19.251:48888/rtsp/1/"
     export default {
         APIURL,
         STREAMSURL,
